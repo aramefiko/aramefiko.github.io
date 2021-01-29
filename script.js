7175f54df716d451cdefd1dbbb9bb2f8f95e18e5
Aramefiko <admin@aramefiko.com>
2021-01-29 23:57:31 +0700

    a

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 $(document).ready(function () {
     $('#nganhhoc').on('change', function () {
-        $.getJSON('data/' + $(this).val(), function (data) {
-                $('#info').text(data.info);
-                $.each(data.subject, function (value) {
-                    $('#subject').append('<li>' + value + '</li>');
-                });
-            }
-        );
+        if ($(this).val()) {
+            $.getJSON('data/' + $(this).val(), function (data) {
+                    $('#info').text(data.info);
+                    $.each(data.subject, function (value) {
+                        $('#subject').append('<li>' + value + '</li>');
+                    });
+                }
+            );
+        }
     });
 });
 
